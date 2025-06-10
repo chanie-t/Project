39521ea8b8310f8f7740da82f797bc87bb33cf7e
chanie-t <Tnguyenthuy288@gmail.com>
2025-06-10 22:35:01 +0700

    author

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CheckRoleUser;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware-> use([
+            CheckRoleUser::class
+        ]);
+         $middleware->alias([
+            'role.check' => CheckRoleUser::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
